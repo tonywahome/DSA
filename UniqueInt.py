fa5d4fee7ac1cb46bf235e35c6d7ba6eb0a5eea6
tonywahome <a.wambugu@alustudent.com>
2024-10-02 18:49:48 +0200

    inputs

--- UniqueInt.py
+++ UniqueInt.py
@@ -2,7 +2,7 @@ class UniqueInt:
     def __init__(self, input_filename, output_filename):
         self.input_filename = input_filename
         self.output_filename = output_filename
-        self.unique_integers = [] 
+        self.unique_integers = []
 
     def is_int(self, s):
         """valid integer checker"""
